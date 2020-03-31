c959702d1993504ead6a66bbb254f8d3e9cbb292
Franni <franziska.haaf@gmail.com>
2020-03-31 12:24:24 +0200

    Added header file for the webpage snippets. Added them into the web interface

--- sittingCubeWebinterface/sittingCubeWebinterface.ino
+++ sittingCubeWebinterface/sittingCubeWebinterface.ino
@@ -2,6 +2,7 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include "wifiAccessData.h"
+#include "webpage-snippets.h"
 
 // Load Wi-Fi library
 #include <ESP8266WiFi.h>
@@ -67,44 +68,34 @@ void loop() {
             client.println("Connection: close");
             client.println();
 
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /5/on") >= 0) {
-              Serial.println("GPIO on");
-              buttoState = "on";
-              digitalWrite(LED_BUILTIN, HIGH);
-            } else if (header.indexOf("GET /5/off") >= 0) {
-              Serial.println("GPIO off");
-              buttoState = "off";
-              digitalWrite(LED_BUILTIN, LOW);
+            //CHANGE THE COLOR -----------------------------------------
+            if (header.indexOf("GET /red/add") >= 0) {
+              Serial.println("Red");
+              //todo: set color
+            } else if (header.indexOf("GET /blue/add") >= 0) {
+              Serial.println("Blue");
+              //todo: set color
+            } else if (header.indexOf("GET /green/add") >= 0) {
+              Serial.println("Green");
+              //todo: set color
             }
 
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}</style></head>");
+            //DISPLAY PAGE -----------------------------------------
+            client.print("<!DOCTYPE html><html>");
+            client.print("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.print("<link rel=\"icon\" href=\"data:,\">");
+            client.print(styleWebpageSnippet);
+            client.print("</head>");
 
-            // Web Page Heading
-            client.println("<body><h1>ESP8266 Web Server</h1>");
-
-            // Display current state, and ON/OFF buttons for GPIO 5
-            client.println("<p>GPIO 5 - State " + buttoState + "</p>");
-            // If the output5State is off, it displays the ON button
-            if (buttoState == "off") {
-              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-
-            client.println("</body></html>");
+            client.println("<body> <div id=\"bottleWrapper\">");
+            client.print(redBottleWebpageSnippet);
+            client.print(blueBottleWebpageSnippet);
+            client.print(greenBottleWebpageSnippet);
+            client.print(resetButtonWebpageSnippet);
+            client.print("</body></html>");
 
             // The HTTP response ends with another blank line
-            client.println();
+            client.print();
             // Break out of the while loop
             break;
           } else { // if you got a newline, then clear currentLine
---
+++ sittingCubeWebinterface/webpage-snippets.h
@@ -0,0 +1,51 @@
+const char* styleWebpageSnippet =
+  "<style>"
+  "html {"
+  "font-family: Helvetica; display: inline-block; margin: 40px auto; text-align: center;}"
+  ".red {fill: #ff0000;}"
+  ".orange { fill: #fca903;}"
+  ".light-orange {fill: #ffcf54;}"
+  ".citrus {fill: #fff833;}"
+  ".yellow {fill: #f8ff33;}"
+  ".green {fill: #00ff00;}"
+  ".turquoise {fill: #00ffdd;}"
+  ".light-blue {fill: #00f2ff;}"
+  ".middle-blue {fill: #00b7ff;}"
+  ".middle-blue-2 {fill: #1cacff;}"
+  ".blue {fill: #0000ff;}"
+  ".slightly-purple-blue {fill: #4733ff;}"
+  ".lilac {fill: #ae52ff;}"
+  ".purple {fill: #ab36ff;}"
+  ".light-purple {fill: #d685ff;}"
+  ".button { background-color: #fff;border: 4px solid #565655;border-radius: 10px;color: #565655;padding: 16px 40px;"
+  "text-decoration: none;font-size: 40px;cursor: pointer;margin-top: 20px;}"
+  ".bottle {display: inline-block; width: 100px;padding: 10px;}"
+  ".svg-outline {fill: none; stroke: #565655;stroke-miterlimit: 10;}"
+  ".svg-white-fill {fill: #fff;}"
+  "</style>";
+
+const char* redBottleWebpageSnippet =
+  "<div class=\"bottle\"><a href=\"/red/add\">"
+  "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 13.04 21.64\">"
+  "</defs><g id=\"Red_Layer_1\" data-name=\"Red_Layer 1\"><g id=\"Red_Layer_2\" data-name=\"Red_Layer 2\">"
+  "<path class=\"svg-outline\" d=\"M9,9.62V2.18h.3a.57.57,0,0,0,.57-.56V1.07A.58.58,0,0,0,9.27.5H4a.57.57,0,0,0-.57.57v.55A.56.56,0,0,0,4,2.18h.22V9.56A6,6,0,1,0,9,9.62Z\" />"
+  "<path class=\"red\" d=\"M8.24,11.34H8V3.91a1,1,0,0,0-1-1h-.8a1,1,0,0,0-1,1V11.3H4.91a4.21,4.21,0,1,0,3.33.05Z\" />"
+  "</g></g></svg></a></div>";
+
+const char* blueBottleWebpageSnippet =
+  "<div class=\"bottle\"><a href=\"/blue/add\">"
+  "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 13.04 21.64\">"
+  "</defs><g id=\"Blue_Layer_1\" data-name=\"Blue_Layer 1\"><g id=\"Blue_Layer_2\" data-name=\"Blue_Layer 2\">"
+  "<path class=\"svg-outline\" d=\"M9,9.62V2.18h.3a.57.57,0,0,0,.57-.56V1.07A.58.58,0,0,0,9.27.5H4a.57.57,0,0,0-.57.57v.55A.56.56,0,0,0,4,2.18h.22V9.56A6,6,0,1,0,9,9.62Z\" />"
+  "<path class=\"blue\" d=\"M8.24,11.34H8V3.91a1,1,0,0,0-1-1h-.8a1,1,0,0,0-1,1V11.3H4.91a4.21,4.21,0,1,0,3.33.05Z\" />"
+  "</g></g></svg></a></div>";
+
+const char* greenBottleWebpageSnippet =
+  "<div class=\"bottle\"><a href=\"/green/add\">"
+  "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 13.04 21.64\">"
+  "</defs><g id=\"Green_Layer_1\" data-name=\"Green_Layer 1\"><g id=\"Green_Layer_2\" data-name=\"Green_Layer 2\">"
+  "<path class=\"svg-outline\" d=\"M9,9.62V2.18h.3a.57.57,0,0,0,.57-.56V1.07A.58.58,0,0,0,9.27.5H4a.57.57,0,0,0-.57.57v.55A.56.56,0,0,0,4,2.18h.22V9.56A6,6,0,1,0,9,9.62Z\" />"
+  "<path class=\"green\" d=\"M8.24,11.34H8V3.91a1,1,0,0,0-1-1h-.8a1,1,0,0,0-1,1V11.3H4.91a4.21,4.21,0,1,0,3.33.05Z\" />"
+  "</g></g></svg></a></div>";
+
+const char* resetButtonWebpageSnippet = "</div><p><a href=\"/reset\"> <button class=\"button\">reset</button></a></p></div>";
